7a2585219181bc316e1d941bad24a0736b4843d7
ManfredSteyer <manfred.steyer@gmx.net>
2017-12-01 14:10:27 +0100

    start

---
+++ src/menu/index.ts
@@ -0,0 +1,45 @@
+import { chain, mergeWith } from '@angular-devkit/schematics';
+import { dasherize, classify } from '@angular-devkit/core';
+import { MenuOptions } from './schema';
+import { apply, filter, move, Rule, template, url, branchAndMerge, Tree, SchematicContext } from '@angular-devkit/schematics';
+import { normalize } from '@angular-devkit/core';
+
+const stringUtils = { dasherize, classify };
+
+function filterTemplates(options: MenuOptions): Rule {
+  if (!options.menuService) {
+    return filter(path => !path.match(/\.service\.ts$/) && !path.match(/-item\.ts$/) && !path.match(/\.bak$/));
+  }
+  return filter(path => !path.match(/\.bak$/));
+}
+
+export default function (options: MenuOptions): Rule {
+
+    // TODO: Validate options and throw SchematicsException if validation fails
+    options.path = options.path ? normalize(options.path) : options.path;
+    
+    const templateSource = apply(url('./files'), [
+        filterTemplates(options),
+        template({
+          ...stringUtils,
+          ...options
+        }),
+        move(options.sourceDir)
+      ]);
+      
+
+      return (host: Tree, context: SchematicContext) => {
+
+        let rule = chain([
+          branchAndMerge(chain([
+            // addDeclarationToModule(),
+            mergeWith(templateSource)
+          ])),
+        ]);
+
+        return rule(host, context);
+  
+
+      }
+
+}
